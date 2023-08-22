e9fe88bd9ebead7a99dbc64979c4d71c0dfd2241
PietroFiori91 <pederflowers@hotmail.it>
2023-08-22 16:07:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+    import TheHeader from "./components/TheHeader.vue0";
+
+    export default {
+      components: {
+        TheHeader,
+      }
+    }
+
+</script>
+
+<template>
+
+  <TheHeader/>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<script>
+
+    export default {}
+
+</script>
+
+<template>
+
+    <div class="container">
+
+        <div class="header">
+
+            <p>culo</p>
+
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
